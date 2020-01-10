59b0858f6a44e0277db0171cc862c2e90a9c4d3c
Steve Newstead <steven.newstead@zonedigital.com>
2020-01-10 12:15:59 +0000

    Quick and dirty webscraper to see if we can feed some data into Google ML to save a ton of manual work

---
+++ PRSData.cs
@@ -0,0 +1,27 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace PRS_ExtractKeyPhrases
+{
+    public class PRSData
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string Persona { get; set; }
+
+        public string Tag1 { get; set; }
+
+        public string Tag2 { get; set; }
+
+        public string Tag3 { get; set; }
+
+        public string Category { get; set; }
+
+ 
+        public string ArtlcieCopy { get;set; }
+
+     
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PRS_ExtractKeyPhrases
+{
+    class Program
+    {
+
+  
+        static void Main(string[] args)
+        {
+            var prsData = GetCSVData();
+            var updatedData = ScrapeySracpeFace(prsData).Result;
+            SaveCSVData(updatedData);
+            Console.ReadLine();
+        }
+
+        protected static IEnumerable<PRSData> GetCSVData()
+        {
+            using var reader = new StreamReader(@"C:\temp\small-data.csv");
+            using var csv = new CsvReader(reader);
+            return csv.GetRecords<PRSData>();
+        }
+
+        protected static void SaveCSVData(IEnumerable<PRSData> prsData)
+        {
+            using (var writer = new StreamWriter(@"c:\temp\output.csv"))
+            {
+                using (var csv = new CsvWriter(writer))
+                {
+                    csv.WriteRecords(prsData);
+                }
+            }
+        }
+        protected static async Task<IEnumerable<PRSData>> ScrapeySracpeFace(IEnumerable<PRSData> prsData)
+        {
+            int counter = 1;
+            foreach (var record in prsData.ToList())
+            {
+                string siteUrl = record.Url;
+
+
+                CancellationTokenSource cancelationToken = new CancellationTokenSource();
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage request = await httpClient.GetAsync(siteUrl);
+                cancelationToken.Token.ThrowIfCancellationRequested();
+
+                Stream response = await request.Content.ReadAsStreamAsync();
+                cancelationToken.Token.ThrowIfCancellationRequested();
+
+                HtmlParser parser = new HtmlParser();
+                IHtmlDocument document = parser.ParseDocument(response);
+
+                record.ArtlcieCopy = GetScrapeResults(document);
+
+                counter++;
+                Console.WriteLine("procecessed: " + counter);
+
+            }
+
+
+            return prsData.ToList();
+
+
+        }
+
+        private static string GetScrapeResults(IHtmlDocument document)
+        {
+            var article = document.All.Where(x => x.ClassName == "article-content").FirstOrDefault().GetElementsByTagName("p");
+            StringBuilder pageCopy = new StringBuilder();
+            foreach (var element in article)
+            {
+                pageCopy.Append(element.Html().Replace(Environment.NewLine, " "));
+            }
+
+            return pageCopy.ToString();
+
+        }
+    }
+}
